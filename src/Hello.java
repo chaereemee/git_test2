c9f10cf2f7306436964ec39db56c097e5873d02a
Lee, Chaerim <leechaerim214@gmail.com>
2023-09-27 10:03:01 +0900

    Hello 클래스 수정

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,14 @@
 public class Hello {
 
 	String message = "æ»≥Á«œººø‰";
+	
+	public Hello() {
+		this("æ»≥Á«œººø‰");
+	}
+	
+	public Hello(String message) {
+		this.message = message;
+	}
 
 	public void say() {
 		System.out.println(message);
